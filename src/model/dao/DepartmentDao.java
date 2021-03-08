8ae6ef12e0937b4a3b65881caade5cef23bdbda1
Felipe-Silva93 <felipesilva4127@gmail.com>
2021-03-08 09:47:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
+	void insert(Department obj);// esse metodo é responssavel por inserir no banco de dados esse objeto(Departamente obj) inviado como parametro de entrada
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);//operação responsavel por pegar o id e consultar no banco de dados o id se exixrtir vai retornar se não vai retornar nulo
+	List<Department>findAll();//fidAll para retornat todos os departamentos então por isso sera uma lista
 
 }
--- src/model/entities/SellerDao.java
+++ src/model/entities/SellerDao.java
@@ -1,5 +1,14 @@
 package model.entities;
 
+import java.util.List;
+
 public interface SellerDao {
+	void insert(Seller obj);// esse metodo é responssavel por inserir no banco de dados esse objeto(SELLER obj) inviado como parametro de entrada
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);//operação responsavel por pegar o id e consultar no banco de dados o id se exixrtir vai retornar se não vai retornar nulo
+	List<Seller>findAll();//fidAll para retornat todos os Seller então por isso sera uma lista
 
 }
+
+
